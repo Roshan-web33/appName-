1530439787cc6f5376cb912379c2cab8aa9ebbcc
Roshan-web33 <roshankumar65688@gamil.com>
2026-04-26 12:38:56 +0530

    add changes

--- README.md
+++
@@ -1,9 +0,0 @@
-# appName-
-Name = Roshan
-<br>
-Task No = 23
-<br>
-This project hlep to under stand the ful concept of responsive web side  .
- 
- Deployment
-app-name-8hpqoc3m4-roshan-web33s-projects.vercel.app
--- script.js
+++
@@ -1,9 +0,0 @@
-const close = document.querySelector("#close")
-const icone = document.querySelector("#icone")
-icone.addEventListener("focus",()=>{
-    close.style.display = "block"
-})
-close.addEventListener('click',()=>{
-    close.style.display = 'none'
-
-})
